6401f7ce4f3ac19a09a4233541dd19f9213e8b70
matthewzareski <78160277+matthewzareski@users.noreply.github.com>
2021-11-24 20:10:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
  function preload(){
      song1 = loadSound("Letgo.mp3");
      song2 = loadSound("river.mp3");
@@ -15,6 +19,24 @@ song2 = "";
  function draw(){
      image(video, 0, 0, 600, 500);
  }
+
+ function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +"leftWristY ="+ leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY ="+ rightWristY);
+    }
+}
+
 
  function start(){
      song1.play();
